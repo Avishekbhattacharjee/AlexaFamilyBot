348919946fce62828aba79dbea060f1b6f86d43c
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:51:21 +0530

    Update torrent.py

--- haruka/modules/torrent.py
+++ haruka/modules/torrent.py
@@ -2,4 +2,4 @@ __help__ = """
  - /torrent <text>: Search for torrent links
  - /torrent download <magnet link | filename>: Downloads a torrent magnet file to the bots server storage. This may be useful as the bot provides 100 GB of free disk space with a download speed of 200 Mbps. After 1 hour the downloaded file will be automatically removed from the server !
 """
-__mod_name__ = "
+__mod_name__ = "Torrent"
