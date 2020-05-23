202809494f1aaa9ea6c2fa291c96322e61919510
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 11:40:35 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -147,9 +147,17 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="Help ❓", callback_data="help_back")]]
+    keyboard = [[
+        InlineKeyboardButton(text=tld(chat.id, "Add me to your group ♥️),
+                             url="t.me/AlexaFamilyBot?startgroup=true")
+    ]]
 
-    update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+    keyboard += [[
+        InlineKeyboardButton(text=tld(chat.id, 'Join our support group 🌍'),
+                             url="https://t.me/AlexaSupport")
+    ]]
+    keyboard += [[InlineKeyboardButton(text="Help ❓", callback_data="help_back")]]
+    update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=False, parse_mode=ParseMode.MARKDOWN)
 
 
 def control_panel(bot, update):
