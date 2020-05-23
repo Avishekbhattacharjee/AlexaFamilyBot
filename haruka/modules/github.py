b3ab278422134d783bf49b6301f76d93e912af0a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:11:37 +0530

    Create github.py

---
+++ haruka/modules/github.py
@@ -0,0 +1,5 @@
+__help__ = """
+ - /git <username>: Returns info about a GitHub user or organization.
+ - /repo <username>: Return the GitHub user or organization repository list 
+"""
+__mod_name__ = "Github"
