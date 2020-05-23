c5f300bf9156c20f28975fabbd63af5c5da3e172
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:22:30 +0530

    Create jfun.py

---
+++ haruka/modules/jfun.py
@@ -0,0 +1,7 @@
+__help__ = """
+ - /cowsay | /tuxsay | /milksay | /kisssay | /wwwsay | /defaultsay | /bunnysay | /moosesay | /sheepsay | /rensay | /cheesesay | /ghostbusterssay | /skeletonsay <text>: Returns a stylish art text from the given text	
+ - /deepfry: Type this in reply to an image/sticker to roast the image/sticker	
+ - /figlet: Another Style art	
+ - /dice: Roll A dice	
+"""
+__mod_name__ = "Just4Fun
