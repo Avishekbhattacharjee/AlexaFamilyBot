e39b178f4c0769e183f2c06433c170abf34adc47
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:14:35 +0530

    Create stt.py

---
+++ haruka/modules/stt.py
@@ -0,0 +1,4 @@
+__help__ = """
+ - /stt <lang>: Type in reply to a voice message to extract text from it.\nn\nFor eg: `/stt en` to a text will extract the text from a the replied voice message in english.
+"""
+__mod_name__ = "Speech2Text"
