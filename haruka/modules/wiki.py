1bcffce44b2907ff9d00cc9f7a9378c22c5c664e
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 09:10:43 +0530

    Update wiki.py

--- haruka/modules/wiki.py
+++ haruka/modules/wiki.py
@@ -2,5 +2,4 @@ __help__ = """
  - /wiki text: Returns search from wikipedia for the input text
 """
 __mod_name__ = "Wikipedia"
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
-dispatcher.add_handler(WIKI_HANDLER)
+
