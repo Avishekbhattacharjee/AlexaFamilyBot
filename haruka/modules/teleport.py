b5d0ee1ccc5b53b4f41b56e8e6bfb3a62df6defd
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 11:30:54 +0530

    Update teleport.py

--- haruka/modules/teleport.py
+++ haruka/modules/teleport.py
@@ -46,7 +46,7 @@ def teleport(bot: Bot, update: Update, args: List[str]):
 
 
 __help__ = """
-/teleport <chatid> <message>: Teleports a message to a specific chat without revealing the sender.Can even send to DM's but the user must have interacted at least once with the bot !
+ - /teleport <chatid> <message>: Teleports a message to a specific chat without revealing the sender.Can even send to DM's but the user must have interacted at least once with the bot !
 """
 __mod_name__ = "Teleport"
 
