5e3f7c72ac29fa654dc94089ef48d409a8063129
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 10:25:38 +0530

    Update English.py

--- haruka/modules/translations/English.py
+++ haruka/modules/translations/English.py
@@ -212,19 +212,16 @@ or feature requests you might have :)
 If you enjoying using me and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """,
 
-    "send-help": """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with.
+    "send-help": """Hello My name is *Alexa* 👩‍🔧
 
-Main commands available:
- - /start: Cool command to check if the bot is alive or not
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /lang: Change bot language
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-   {}
+I'm here to help you manage your groups 🤖
+
+Click Help button to get my commands list 😊
+
+Found any bug in the bot ? Feel free to share in our Support Group 🌍
+
+`#include <std/disclaimer.h>`
+[https://telegra.ph/ALEXA](https://telegra.ph/ALEXA-05-19)
    """,
 
     "send-group-settings": """Hi there! There are quite a few settings for *{}* - go ahead and pick what
