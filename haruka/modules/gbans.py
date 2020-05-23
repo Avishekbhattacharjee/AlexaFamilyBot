d31051ef938443e868512c17fc9fb681362b8447
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:02:39 +0530

    Create gbans.py

---
+++ haruka/modules/gbans.py
@@ -0,0 +1,5 @@
+__help__ = """
+*ONLY FOR SUDO USERS*
+ - /gban: Type in reply to a person to globally ban that person across all the chats where this bot is admin
+"""
+__mod_name__ = "Global Bans"
