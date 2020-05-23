1039af20e15373124a58bed82bd21963ed28c795
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 19:02:55 +0530

    Update term.py

--- haruka/modules/term.py
+++ haruka/modules/term.py
@@ -1,4 +1,4 @@
 __help__ = """
- - /term <lang> <commands>: A micro terminal environment for executing basic commands.Delimeters should be in a single line !\n\nExample Usage: `/term bash date`\n\nFor multiple delimeters use *&&* eg: `term bash echo "hi, how are you ?" > hi.txt && cat hi.txt`
+ - /term <lang> <commands>: A micro terminal environment for executing basic commands.Delimeters should be in a single line !\n\nExample Usage: `/term date`\n\nFor multiple delimeters use *&&* eg: `term echo "hi, how are you ?" > hi.txt && cat hi.txt` 
 """
 __mod_name__ = "Terminal"
