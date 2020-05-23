65912d551534f6e7fb8fc542db073f4f33c5f067
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:06:42 +0530

    Create gmute.py

---
+++ haruka/modules/gmute.py
@@ -0,0 +1,7 @@
+__help__ = """
+*ONLY FOR SUDO USERS*
+ - /gmute: Type in reply to a person to globally mute that person across all the chats where this bot is admin
+*FOR EVERYONE*
+ - /gmutelist: List all the persons who are globally muted 
+"""
+__mod_name__ = "Global Mutes"
