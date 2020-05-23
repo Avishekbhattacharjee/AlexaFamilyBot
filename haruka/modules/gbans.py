d475c20e4cf32c272fee95239696cb15bb04eeeb
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:07:21 +0530

    Update gbans.py

--- haruka/modules/gbans.py
+++ haruka/modules/gbans.py
@@ -1,5 +1,7 @@
 __help__ = """
 *ONLY FOR SUDO USERS*
  - /gban: Type in reply to a person to globally ban that person across all the chats where this bot is admin
+*FOR EVERYONE*
+ - /gbanlist: List all the persons who are globally banned 
 """
 __mod_name__ = "Global Bans"
