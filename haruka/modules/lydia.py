16cf2ce0e1bb79a79ace3b2cb0ff294b57a29301
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-23 12:19:20 +0530

    Create lydia.py

---
+++ haruka/modules/lydia.py
@@ -0,0 +1,5 @@
+__help__ = """
+ - /autochat: Type in reply to someone to activate on him/her. The bot will give auto replies to that person.
+ - /stopchat: Type in reply to someone to deactivate on him/her. The bot will stop giving auto replies to that person.
+"""
+__mod_name__ = "Lydia"
