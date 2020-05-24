1c4cbd54ca43b98a4ba2fb907d087466018c0074
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-25 00:11:58 +0530

    Update blacklist.py

--- haruka/modules/blacklist.py
+++ haruka/modules/blacklist.py
@@ -17,6 +17,17 @@ from haruka.modules.connection import connected
 
 from haruka.modules.translations.strings import tld
 
+import html
+
+from telegram import Bot, ParseMode, Update
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
+
+import tldextract
+from haruka import LOGGER, dispatcher
+from haruka.modules.disable import DisableAbleCommandHandler
+from haruka.modules.helper_funcs.chat_status import user_admin, user_not_admin
+
 BLACKLIST_GROUP = 11
 
 
@@ -182,18 +193,6 @@ def __chat_settings__(bot, update, chat, chatP, user):
 def __stats__():
     return "{} blacklist triggers, across {} chats.".format(sql.num_blacklist_filters(),
                                                             sql.num_blacklist_filter_chats())
-import html
-
-from telegram import Bot, ParseMode, Update
-from telegram.error import BadRequest
-from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
-
-import tldextract
-from haruka import LOGGER, dispatcher
-from haruka.modules.disable import DisableAbleCommandHandler
-from haruka.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from haruka.modules.sql import urlblacklist_sql as sql
-
 
 @run_async
 @user_admin
@@ -319,38 +318,6 @@ def get_blacklisted_urls(bot: Bot, update: Update):
     message.reply_text(base_string, parse_mode=ParseMode.HTML)
 
 
-URL_BLACKLIST_HANDLER = DisableAbleCommandHandler(
-    "blacklist",
-    add_blacklist_url,
-    filters=Filters.group,
-    pass_args=True,
-    admin_ok=True)
-ADD_URL_BLACKLIST_HANDLER = CommandHandler(
-    "addurl", add_blacklist_url, filters=Filters.group)
-
-RM_BLACKLIST_URL_HANDLER = CommandHandler(
-    "delurl", rm_blacklist_url, filters=Filters.group)
-
-GET_BLACKLISTED_URLS = CommandHandler(
-    "geturl",
-    get_blacklisted_urls,
-    filters=Filters.group)
-
-URL_DELETE_HANDLER = MessageHandler(
-    Filters.entity("url"),
-    del_blacklist_url,
-    edited_updates=True)
-
-
-dispatcher.add_handler(URL_BLACKLIST_HANDLER)
-dispatcher.add_handler(ADD_URL_BLACKLIST_HANDLER)
-dispatcher.add_handler(RM_BLACKLIST_URL_HANDLER)
-dispatcher.add_handler(GET_BLACKLISTED_URLS)
-dispatcher.add_handler(URL_DELETE_HANDLER)
-
-
-__mod_name__ = "Blacklists"
-
 __help__ = """
 You can set blacklist filters to take automatic action on people when they say certain things. This is done using:
  - /addblacklist <blacklist trigger> <blacklist reason>: blacklists the trigger. You can set sentences by putting quotes around the reason.
@@ -383,8 +350,9 @@ the message will immediately be deleted.
 
 - /addurl <urls>: Add a domain to the blacklist. The bot will automatically parse the url.
 - /delurl <urls>: Remove urls from the blacklist
-
 """
+__mod_name__ = "Blacklists"
+
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler("blacklist", blacklist, pass_args=True, admin_ok=True)
 ADD_BLACKLIST_HANDLER = CommandHandler("addblacklist", add_blacklist)
@@ -396,3 +364,32 @@ dispatcher.add_handler(BLACKLIST_HANDLER)
 dispatcher.add_handler(ADD_BLACKLIST_HANDLER)
 dispatcher.add_handler(UNBLACKLIST_HANDLER)
 dispatcher.add_handler(BLACKLIST_DEL_HANDLER, group=BLACKLIST_GROUP)
+URL_BLACKLIST_HANDLER = DisableAbleCommandHandler(
+    "blacklist",
+    add_blacklist_url,
+    filters=Filters.group,
+    pass_args=True,
+    admin_ok=True)
+ADD_URL_BLACKLIST_HANDLER = CommandHandler(
+    "addurl", add_blacklist_url, filters=Filters.group)
+
+RM_BLACKLIST_URL_HANDLER = CommandHandler(
+    "delurl", rm_blacklist_url, filters=Filters.group)
+
+GET_BLACKLISTED_URLS = CommandHandler(
+    "geturl",
+    get_blacklisted_urls,
+    filters=Filters.group)
+
+URL_DELETE_HANDLER = MessageHandler(
+    Filters.entity("url"),
+    del_blacklist_url,
+    edited_updates=True)
+
+
+dispatcher.add_handler(URL_BLACKLIST_HANDLER)
+dispatcher.add_handler(ADD_URL_BLACKLIST_HANDLER)
+dispatcher.add_handler(RM_BLACKLIST_URL_HANDLER)
+dispatcher.add_handler(GET_BLACKLISTED_URLS)
+dispatcher.add_handler(URL_DELETE_HANDLER)
+
