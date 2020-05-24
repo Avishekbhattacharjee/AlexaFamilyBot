7cace6e20539886162a5361d4552692cf17cede8
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-24 11:37:50 +0530

    Update misc.py

--- haruka/modules/helper_funcs/misc.py
+++ haruka/modules/helper_funcs/misc.py
@@ -1,12 +1,11 @@
-from math import ceil
+from functools import wraps
 from typing import List, Dict
 
-from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode, Update
 from telegram.error import TelegramError
 
-from haruka import LOAD, NO_LOAD
-from haruka.modules.translations.strings import tld
-from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+from haruka import LOAD, NO_LOAD, OWNER_ID
+from haruka.modules.tr_engine.strings import tld
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -41,46 +40,65 @@ def split_message(msg: str) -> List[str]:
         return result
 
 
-def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None) -> List:
+def paginate_modules(chat_id,
+                     page_n: int,
+                     module_dict: Dict,
+                     prefix,
+                     chat=None) -> List:
     if not chat:
-        modules = sorted(
-            [EqInlineKeyboardButton(tld(chat_id, x.__mod_name__),
-                                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(tld(chat_id, "modname_" + x),
+                                   callback_data="{}_module({})".format(
+                                       prefix, x)) for x in module_dict.keys()
+        ])
     else:
-        modules = sorted(
-            [EqInlineKeyboardButton(tld(chat_id, x.__mod_name__),
-                                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
-             in module_dict.values()])
+        modules = sorted([
+            EqInlineKeyboardButton(tld(chat_id, "modname_" + x),
+                                   callback_data="{}_module({},{})".format(
+                                       prefix, chat, x))
+            for x in module_dict.keys()
+        ])
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = [
+        modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
+    ]
 
-    if len(modules) % 2 == 1:
-        pairs.append((modules[-1],))
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
+        pairs.append((modules[-1], ))
+    elif calc == 2:
+        pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
+    # max_num_pages = ceil(len(pairs) / 28)
+    # modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
-             EqInlineKeyboardButton(">>", callback_data="{}_next({})".format(prefix, modulo_page)))]
-    else:
-        pairs += [[EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]]
 
+    #if len(pairs) > 21:
+    #    pairs = pairs[modulo_page * 28:28]
+    # else:
+    #     pairs += [[
+    #         EqInlineKeyboardButton(tld(chat_id, '🇧 🇦 🇨 🇰',
+    #                                callback_data="PM_START")
+    #     ]]
 
     return pairs
 
 
-def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
+def send_to_list(bot: Bot,
+                 send_to: list,
+                 message: str,
+                 markdown=False,
+                 html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
         try:
             if markdown:
-                bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
+                bot.send_message(user_id,
+                                 message,
+                                 parse_mode=ParseMode.MARKDOWN)
             elif html:
                 bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
             else:
@@ -123,4 +141,5 @@ def user_bot_owner(func):
             return func(bot, update, *args, **kwargs)
         else:
             pass
+
     return is_user_bot_owner
