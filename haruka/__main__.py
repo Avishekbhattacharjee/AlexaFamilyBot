b8b55db029bc7476cece4c18f0a3972a6a43c5c8
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-24 13:46:59 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -392,7 +392,7 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="Help",
+                                                [[InlineKeyboardButton(text="My Commands ⚙️",
                                                                        url="t.me/{}?start=help".format(
                                                                            bot.username))]]))
         return
@@ -406,7 +406,7 @@ def get_help(bot: Bot, update: Update):
             help_txt = HELPABLE[module].__help__
 
         text = tld(chat.id, "Here is the help for the *{}* module:\n{}").format(mod_name, help_txt)
-        send_help(chat.id, text, InlineKeyboardMarkup([[InlineKeyboardButton(text=tld(chat.id, "Back"), callback_data="help_back")]]))
+        send_help(chat.id, text, InlineKeyboardMarkup([[InlineKeyboardButton(text=tld(chat.id, "🅱🅰🅲🅺"), callback_data="help_back")]]))
 
     else:
         send_help(chat.id, tld(chat.id, "send-help").format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else tld(
@@ -460,7 +460,7 @@ def settings_button(bot: Bot, update: Update):
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton(text="Back",
+                                         [[InlineKeyboardButton(text="🅱🅰🅲🅺",
                                                                 callback_data="stngs_back({})".format(chat_id))]]))
 
         elif prev_match:
