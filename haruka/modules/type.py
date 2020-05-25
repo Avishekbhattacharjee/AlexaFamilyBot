54016d9e28244b79a71967803c88478feaa39899
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-25 14:20:43 +0530

    Update type.py

--- haruka/modules/type.py
+++ haruka/modules/type.py
@@ -8,42 +8,35 @@ import html
 import random
 import time
 from typing import List
-
 from telegram import Bot, Update, ParseMode
 from telegram.ext import run_async
-
 from haruka import dispatcher
 from haruka.modules.helper_funcs.chat_status import is_user_admin, user_admin
 from haruka.modules.helper_funcs.extraction import extract_user
 
 
-@register(pattern='^/type(?: |$)(.*)')
+@register(pattern="^/type (.*)")
 async def typewriter(typew):
-    """ Just a small command to make your keyboard become a typewriter! """
-    textx = await typew.get_reply_message()
     message = typew.pattern_match.group(1)
     if message:
         pass
-    elif textx:
-        message = textx.text
     else:
         await typew.reply("`Give a text to type!`")
         return
-    sleep_time = 1
     typing_symbol = "|"
     old_text = ''
     now = await typew.reply(typing_symbol)
-    await asyncio.sleep(sleep_time)
+    await asyncio.sleep(2)
     for character in message:
         old_text = old_text + "" + character
         typing_text = old_text + "" + typing_symbol
         await now.edit(typing_text)
-        await asyncio.sleep(sleep_time)
+        await time.sleep(2)
         await now.edit(old_text)
-        await asyncio.sleep(sleep_time)
+        await time.sleep(2)
 
 
 __help__ = """
- - /type <text>: Make the bot type something for you in a professional way 
+ - /type <text>: Make the bot type something for you in a professional way
 """
 __mod_name__ = "Typewriter"
