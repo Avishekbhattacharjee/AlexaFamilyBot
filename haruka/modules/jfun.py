5fdbf653dabf100b5447c7f93823c30b2949b934
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-26 18:48:16 +0530

    Update jfun.py

--- haruka/modules/jfun.py
+++ haruka/modules/jfun.py
@@ -3,6 +3,8 @@ __help__ = """
  - /deepfry: Type this in reply to an image/sticker to roast the image/sticker	
  - /figlet: Another Style art	
  - /dice: Roll A dice	
+ - /dart: Throw a dart and try your luck
+ - /basketball: Try your luck if you can enter the ball in the ring
  - /deepfry: Roast a image/sticker
 """
 __mod_name__ = "Just4Fun"
