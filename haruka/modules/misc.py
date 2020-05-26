876cd2138cec97350a19892ac8d18f2d0ecfa216
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-27 00:37:06 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1544,6 +1544,7 @@ def gettime(bot: Bot, update: Update):
     except:
         message.reply_text("Provide a country name/abbreviation/timezone to find.")
         return
+    send_message = message.reply_text(f"Finding timezone info for <b>{query}</b>", parse_mode=ParseMode.HTML)
 
     query_timezone = query.lower()
     if len(query_timezone) == 2:
@@ -1558,6 +1559,7 @@ def gettime(bot: Bot, update: Update):
     send_message.edit_text(result, parse_mode=ParseMode.HTML)
 
 
+
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -1568,111 +1570,83 @@ from time import sleep
 import asyncio
 import os
 import random
-@register(pattern=r"^/carbon")
+import os
+import time
+import asyncio
+import shutil
+from bs4 import BeautifulSoup
+import re
+from time import sleep
+from html import unescape
+from re import findall
+from selenium import webdriver
+from urllib.parse import quote_plus
+from urllib.error import HTTPError
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+@register(pattern="^/carbon")
 async def carbon_api(e):
- RED = random.randint(0,256)
- GREEN = random.randint(0,256)
- BLUE = random.randint(0,256)
- THEME= [         "3024-night",
-                  "a11y-dark",
-                  "blackboard",
-                  "base16-dark",
-                  "base16-light",
-                  "cobalt",
-                  "dracula",
-                  "duotone-dark",
-                  "hopscotch",
-                  "lucario",
-                  "material",
-                  "monokai",
-                  "night-owl",
-                  "nord",
-                  "oceanic-next",
-                  "one-light",
-                  "one-dark",
-                  "panda-syntax",
-                  "paraiso-dark",
-                  "seti",
-                  "shades-of-purple",
-                  "solarized",
-                  "solarized%20light",
-                  "synthwave-84",
-                  "twilight",
-                  "verminal",
-                  "vscode",
-                  "yeti",
-                  "zenburn",
-]
+    """ A Wrapper for carbon.now.sh """
+    gotta = await e.reply("`Processing..`")
+    CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
+    global CARBONLANG
+    textx = await e.get_reply_message()
+    pcode = e.text
+    if pcode[8:]:
+        pcode = str(pcode[8:])
+    elif textx:
+        pcode = str(textx.message)  # Importing message to module
+    code = quote_plus(pcode)  # Converting to urlencoded
+    await gotta.edit("`Processing..\n25%`")
+    if os.path.isfile("./carbon.png"):
+        os.remove("./carbon.png")
+    url = CARBON.format(code=code, lang=CARBONLANG)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    prefs = {'download.default_directory': './'}
+    chrome_options.add_experimental_option('prefs', prefs)
+    driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
+                              options=chrome_options)
+    driver.get(url)
+    await gotta.edit("`Processing..\n50%`")
+    download_path = './'
+    driver.command_executor._commands["send_command"] = (
+        "POST", '/session/$sessionId/chromium/send_command')
+    params = {
+        'cmd': 'Page.setDownloadBehavior',
+        'params': {
+            'behavior': 'allow',
+            'downloadPath': download_path
+        }
+    }
+    command_result = driver.execute("send_command", params)
+    driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
+    driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
+    driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
+    await gotta.edit("`Processing..\n75%`")
+    # Waiting for downloading
+    while not os.path.isfile("./carbon.png"):
+        await sleep(0.5)
+    await gotta.edit("`Processing..\n100%`")
+    file = './carbon.png'
+    await gotta.edit("`Uploading..`")
+    await e.client.send_file(
+        e.chat_id,
+        file,
+        caption="<===@AlexaFamilyBot===>",
+        force_document=True,
+        reply_to=e.message.reply_to_msg_id,
+    )
 
- CUNTHE = random.randint(0, len(THEME) - 1)
- The = THEME[CUNTHE]
-
-
- if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-   """ A Wrapper for carbon.now.sh """
-   golund = await e.reply("▢▢▢▢▢▢")
-   CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
-   CARBONLANG = "en"
-   textx = await e.get_reply_message()
-   pcode = e.text
-   if pcode[8:]:
-         pcodee = str(pcode[8:])
-         if "|" in pcodee:
-               pcode, skeme = pcodee.split("|")
-         else:
-               pcode = pcodee
-               skeme = None
-   elif textx:
-         pcode = str(textx.message) 
-         skeme = None # Importing message to module
-   code = quote_plus(pcode) # Converting to urlencoded# Importing message to module
-   code = quote_plus(pcode) # Converting to urlencoded
-   url = CARBON.format(code=code, R=RED, G=GREEN, B=BLUE, T=The, lang=CARBONLANG)
-   chrome_options = Options()
-   chrome_options.add_argument("--headless")
-   chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-   chrome_options.add_argument("--window-size=1920x1080")
-   chrome_options.add_argument("--disable-dev-shm-usage")
-   chrome_options.add_argument("--no-sandbox")
-   chrome_options.add_argument('--disable-gpu')
-   prefs = {'download.default_directory' : './'}
-   chrome_options.add_experimental_option('prefs', prefs)
-   await golund.edit("▣▣▢▢▢▢")
-
-   driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
-   driver.get(url)
-   download_path = './'
-   driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-   params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
-   command_result = driver.execute("send_command", params)
-
-   driver.find_element_by_id("export-menu").click()
-
-   #removing below line coz seems no use now
-   #driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-
-   sleep(5) # this might take a bit.
-   await golund.edit("▣▣▣▢▢▢")
-   driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-   sleep(5)
-   await golund.edit("▣▣▣▣▢▢")
-   driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-   sleep(5) #Waiting for downloading
-   await golund.edit("▣▣▣▣▣▢")
-
-   file = './carbon.png'
-   caption = "**Carbon\nRGB Colour Code: `({}, {}, {})`\nTheme: `{}`**".format(RED, GREEN, BLUE, The)
-   await golund.edit("▣▣▣▣▣▣\n**Carbon Completed.\nUploading...**")
-   await e.client.send_file(
-         e.chat_id,
-         file,
-         caption=caption,
-         force_document=True,
-         reply_to=e.message.reply_to_msg_id,
-         )
-
-   os.remove('./carbon.png')
-   # Removing carbon.png after uploading
+    os.remove('./carbon.png')
+    driver.quit()
+    # Removing carbon.png after uploading
 
 
 
@@ -1709,7 +1683,7 @@ PASTE_HANDLER = DisableAbleCommandHandler("paste", paste, pass_args=True)
 GET_PASTE_HANDLER = DisableAbleCommandHandler("getpaste", get_paste_content, pass_args=True)
 PASTE_STATS_HANDLER = DisableAbleCommandHandler("pastestats", get_paste_stats, pass_args=True)
 LYRICS_HANDLER = CommandHandler("lyrics", lyrics, pass_args=True)
-TIME_HANDLER = CommandHandler("time", gettime)
+TIME_HANDLER = CommandHandler("datetime", gettime)
 
 dispatcher.add_handler(TIME_HANDLER)
 dispatcher.add_handler(PASTE_HANDLER)
