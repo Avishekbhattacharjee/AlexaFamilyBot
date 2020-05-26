1e0864ccc1725576289b4f4dc598a790e600a39f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-27 00:16:17 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1558,6 +1558,123 @@ def gettime(bot: Bot, update: Update):
     send_message.edit_text(result, parse_mode=ParseMode.HTML)
 
 
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from telethon import events
+from urllib.parse import quote_plus
+from urllib.error import HTTPError
+from time import sleep
+import asyncio
+import os
+import random
+@register(pattern=r"^/carbon")
+async def carbon_api(e):
+ RED = random.randint(0,256)
+ GREEN = random.randint(0,256)
+ BLUE = random.randint(0,256)
+ THEME= [         "3024-night",
+                  "a11y-dark",
+                  "blackboard",
+                  "base16-dark",
+                  "base16-light",
+                  "cobalt",
+                  "dracula",
+                  "duotone-dark",
+                  "hopscotch",
+                  "lucario",
+                  "material",
+                  "monokai",
+                  "night-owl",
+                  "nord",
+                  "oceanic-next",
+                  "one-light",
+                  "one-dark",
+                  "panda-syntax",
+                  "paraiso-dark",
+                  "seti",
+                  "shades-of-purple",
+                  "solarized",
+                  "solarized%20light",
+                  "synthwave-84",
+                  "twilight",
+                  "verminal",
+                  "vscode",
+                  "yeti",
+                  "zenburn",
+]
+
+ CUNTHE = random.randint(0, len(THEME) - 1)
+ The = THEME[CUNTHE]
+
+
+ if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+   """ A Wrapper for carbon.now.sh """
+   golund = await e.reply("▢▢▢▢▢▢")
+   CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
+   CARBONLANG = "en"
+   textx = await e.get_reply_message()
+   pcode = e.text
+   if pcode[8:]:
+         pcodee = str(pcode[8:])
+         if "|" in pcodee:
+               pcode, skeme = pcodee.split("|")
+         else:
+               pcode = pcodee
+               skeme = None
+   elif textx:
+         pcode = str(textx.message) 
+         skeme = None # Importing message to module
+   code = quote_plus(pcode) # Converting to urlencoded# Importing message to module
+   code = quote_plus(pcode) # Converting to urlencoded
+   url = CARBON.format(code=code, R=RED, G=GREEN, B=BLUE, T=The, lang=CARBONLANG)
+   chrome_options = Options()
+   chrome_options.add_argument("--headless")
+   chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+   chrome_options.add_argument("--window-size=1920x1080")
+   chrome_options.add_argument("--disable-dev-shm-usage")
+   chrome_options.add_argument("--no-sandbox")
+   chrome_options.add_argument('--disable-gpu')
+   prefs = {'download.default_directory' : './'}
+   chrome_options.add_experimental_option('prefs', prefs)
+   await golund.edit("▣▣▢▢▢▢")
+
+   driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
+   driver.get(url)
+   download_path = './'
+   driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+   params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
+   command_result = driver.execute("send_command", params)
+
+   driver.find_element_by_id("export-menu").click()
+
+   #removing below line coz seems no use now
+   #driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
+
+   sleep(5) # this might take a bit.
+   await golund.edit("▣▣▣▢▢▢")
+   driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
+   sleep(5)
+   await golund.edit("▣▣▣▣▢▢")
+   driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
+   sleep(5) #Waiting for downloading
+   await golund.edit("▣▣▣▣▣▢")
+
+   file = './carbon.png'
+   caption = "**Carbon\nRGB Colour Code: `({}, {}, {})`\nTheme: `{}`**".format(RED, GREEN, BLUE, The)
+   await golund.edit("▣▣▣▣▣▣\n**Carbon Completed.\nUploading...**")
+   await e.client.send_file(
+         e.chat_id,
+         file,
+         caption=caption,
+         force_document=True,
+         reply_to=e.message.reply_to_msg_id,
+         )
+
+   os.remove('./carbon.png')
+   # Removing carbon.png after uploading
+
+
 
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
