76cee93b8c79f5fb3e2dd26d6f2dff5f98b85c03
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-26 21:42:15 +0530

    Update gkick.py

--- haruka/modules/gkick.py
+++ haruka/modules/gkick.py
@@ -1,5 +1,5 @@
 __help__ = """
 *ONLY FOR SUDO USERS*
- - /gban: Type in reply to a person to globally kick that person across all the chats where this bot is admin
+ - /gkick: Type in reply to a person to globally kick that person across all the chats where this bot is admin
 """
 __mod_name__ = "Global Kicks"
