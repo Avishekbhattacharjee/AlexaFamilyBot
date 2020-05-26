505717f307313681cede7873e51448ab6314605f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-26 18:28:21 +0530

    Update news.py

--- haruka/modules/news.py
+++ haruka/modules/news.py
@@ -1,4 +1,4 @@
 __help__ = """
- - /news: Returns today's headlines\nMay cause flood in the chat so better use in PM's
+ - /news: Returns today's headlines\nMay cause flood in the chat so better use in PM
 """
 __mod_name__ = "News"
