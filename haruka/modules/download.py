3374fac48896f0a8fc1144b34926a04fd247b389
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-27 23:33:23 +0530

    Update download.py

--- haruka/modules/download.py
+++ haruka/modules/download.py
@@ -9,19 +9,15 @@ from telethon import events
 from telethon.tl.types import DocumentAttributeVideo
 
 
-@register(pattern="^/download (.*)")
+@register(pattern="^/download")
 async def _(event):
     if event.fwd_from:
         return
     mone = await event.reply("Processing ...")
-    input_str = event.pattern_match.group(1)
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
-        start = datetime.now()
+        start = datetime.datetime.now()
         reply_message = await event.get_reply_message()
         try:
-            c_time = time.time()
             downloaded_file_name = await event.client.download_media(
                 reply_message,
                 TEMP_DOWNLOAD_DIRECTORY)
@@ -31,3 +27,8 @@ async def _(event):
             end = datetime.datetime.now()
             ms = (end - start).seconds
             await mone.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+
+__help__ = """
+ - /download: Type in reply to a telegram document/audio/video to download to the bots local server
+"""
+__mod_name__ = True
