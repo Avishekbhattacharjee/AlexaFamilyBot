201313a8427fca30a86b33d737cb52a55d94250d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-28 21:58:11 +0530

    Update yt.py

--- haruka/modules/yt.py
+++ haruka/modules/yt.py
@@ -1,4 +1,5 @@
 __help__ = """
  - /yt <text>: perform a youtube search
+ - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local storage
 """
 __mod_name__ = "Youtube"
