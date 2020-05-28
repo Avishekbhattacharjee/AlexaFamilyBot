c4cf48930af863fd16cfe60449b9da663054355d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-28 12:27:50 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1277,7 +1277,7 @@ async def terminal_runner(term):
     except ImportError:
         uid = "This ain't it chief!"
 
-    if command in ("crowdin.yml", "deeppyer", "Dockerfile", "haruka", "images", "LICENSE", "locales", "nltk_data", "Procfile", "README.md", "requirements.txt", "runtime.txt", "config.env"):
+    if command in ("/*", "/", "crowdin.yml", "deeppyer", "Dockerfile", "haruka", "images", "LICENSE", "locales", "nltk_data", "Procfile", "README.md", "requirements.txt", "runtime.txt", "config.env"):
         await term.reply("`That's a dangerous operation! Not Permitted!`")
         return
 
