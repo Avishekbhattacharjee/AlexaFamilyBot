25f0cdf86a6e2105d3e64589812f3c1297b52c0b
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-29 22:55:42 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1588,94 +1588,22 @@ async def wolfram(wvent):
     server = f'https://api.wolframalpha.com/v1/spoken?appid={appid}&i={i}'
     res = get(server)
     await wvent.reply(f'**{i}**\n\n' + res.text, parse_mode='Markdown')
-   
 
-"""
-Torrent Search Plugin for Userbot. //torrentdownloads.me
-cmd: .search search_string
-Note: Number of results are currently limited to 30
-By:-@Zero_cool7870
 
-"""
-from bs4 import BeautifulSoup as bs 
-import requests
-from telethon import events
-import asyncio
-from telegraph import Telegraph
-
-telegraph = Telegraph()
-telegraph.create_account(short_name='Alexa')
-
+from pyYify import yify
 
 @register(pattern="^/torrent (.*)")
 async def tor_search(event):
 	if event.fwd_from:
 		return 
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
-
-	search_str = event.pattern_match.group(1)
-
-	print(search_str)
-	await event.reply("Searching for "+search_str+".....")
-	if " " in search_str:
-		search_str = search_str.replace(" ","+")
-		print(search_str)
-		res = requests.get("https://www.limetorrents.info/search/?new=1&s_cat=0&search="+search_str,headers)
-
-	else:
-		res = requests.get("https://www.limetorrents.info/search/?search="+search_str,headers)
-
-	source = bs(res.text,'lxml')
-	with open("source.html",'w') as f:
-		f.write(str(source))
-
-	urls = []
-	magnets = []
-
-	counter = 0
-	for a in source.find_all('a',{'class':'cloud'}):
-		# print("https://www.limetorrents.info"+a['href'])
-		try:
-			urls.append("https://www.limetorrents.info"+a['href'])
-		except:
-			pass
-		if counter == 30:
-			break		
-		counter = counter + 1
-	if not urls:
-		await event.reply("Either the Keyword was restricted or not found..")		
-		return
-
-	print("Found URLS....")
-		
-	for url in urls:
-		res = requests.get(url,headers)
-		# print("URl: "+url)
-		source = bs(res.text,'lxml')
-		with open("source2.html",'w') as f:
-			f.write(str(source))
-		for div in source.find_all('div',{'class':'grey_bar1 back_none'}):
-			try:
-				mg = div.p.a['href']
-				# print(str(mg))
-				magnets.append("<b>URL: </b>"+str(url)+"<br/><b>\nMagnet: </b>{}\n<br/>".format(str(mg)))
-			except:
-				pass	
-	print("Found Magnets...")			
-	msg = ""
-	try:
-		search_str = search_str.replace("+"," ")
-	except:
-		pass	
-	for i in magnets:
-		msg = msg + i
-	response = telegraph.create_page(
-	search_str,
-	html_content = msg
-	)	
-	await event.reply('Magnet Links for {}:\nhttps://telegra.ph/{}'.format(search_str,response['path']))
-
-
+        input = event.pattern_match.group(1)
+        node = f"'{input}'"
+	movies_list = yify.search_movies(node)
+        movie1 = movies_list[0]
+        torrent1 = movie1.torrents[0]
+        oook = torrent1.magnet
+        fuckoff = f"`{oook}`"
+        await event.reply("**Magnet Link:** "+fuckoff)
 
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
