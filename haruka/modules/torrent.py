a61b749152ced73ec9be45cb26253f0e37f5c4f1
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-05-29 22:57:28 +0530

    Update torrent.py

--- haruka/modules/torrent.py
+++ haruka/modules/torrent.py
@@ -1,4 +1,5 @@
 __help__ = """
+*NOTE: AFTER YOU GET THE TORRENT(MAGNET) LINK USE ANY TORRENT DOWNLOADER LIKE BITTORRENT, UTORRENT ETC. TO DOWNLOAD YOUR STUFF*
  - /torrent <text>: Search for torrent links
 """
 __mod_name__ = "Torrent"
