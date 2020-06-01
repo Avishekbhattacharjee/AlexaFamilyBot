2ff7ca04be24adcfb9c59b8195bf08570b51b5e9
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-01 21:36:33 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1601,6 +1601,7 @@ async def tor_search(event):
       await event.client.send_file(
             event.chat_id,
             "torrent.json",
+            caption=f"Here is the list of torrents for **{input}**"
             reply_to=event.message.reply_to_msg_id)
       os.system('rm -rf torrent.json')
 
