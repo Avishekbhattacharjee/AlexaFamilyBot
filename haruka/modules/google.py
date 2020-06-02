e7ff185a8b80082926e3f1f3bc83ed74048fdff0
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-02 12:52:42 +0530

    Update google.py

--- haruka/modules/google.py
+++ haruka/modules/google.py
@@ -1,5 +1,4 @@
 __help__ = """
  - /google <text>: perform a google search
- - /alexa <question>: Ask alexa any question and it will give accurate reply. For eg: `/alexa where is Taj Mahal`, `/alexa what is the age of Virat Kohli` etc..
 """
 __mod_name__ = "Google Search"
