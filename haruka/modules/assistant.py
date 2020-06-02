247d3635b233750509ed9f0df57a702b2f5164d5
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-02 12:56:51 +0530

    Update assistant.py

--- haruka/modules/assistant.py
+++ haruka/modules/assistant.py
@@ -1,4 +1,7 @@
 __help__ = """
  - /alexa <question>: Ask alexa any question and it will give accurate reply. For eg: `/alexa where is Taj Mahal`, `/alexa what is the age of Virat Kohli` etc..
+
+*NOTE*
+The question should be a meaningful one otherwise you will get no response !
 """
 __mod_name__ = "Assistant"
