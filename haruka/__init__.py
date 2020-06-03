06f87d8de661e01bf11c6a7f99d9727652fa600f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-03 22:47:05 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -67,7 +67,7 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     WOLFRAM_ID = os.environ.get('WOLFRAM_ID', None)
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADEAgAAgi3GQL9YQyT_kBpQwI')
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', True)
+    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     SUDO_USERS.add(OWNER_ID)
     GBAN_LOGS = os.environ.get('GBAN_LOGS', None)
     tbot = TelegramClient("haruka", API_KEY, API_HASH)
