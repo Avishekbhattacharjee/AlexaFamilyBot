f2e12fdccb20ce74fceda8f985677bab284f2a16
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-06 15:56:41 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1510,25 +1510,84 @@ async def wolfram(wvent):
     answer = next(res.results).text 
     await wvent.reply(f'**{question}**\n\n' + answer, parse_mode='Markdown')
     
+from bs4 import BeautifulSoup as bs 
+import requests
+from telethon import events
+from haruka.events import register
+import asyncio
+from telegraph import Telegraph
 
-import subprocess
+telegraph = Telegraph()
+telegraph.create_account(short_name='@AlexaFamilyBot')
+
 
 @register(pattern="^/torrent (.*)")
 async def tor_search(event):
-      if event.fwd_from:
-         return 
-      input = event.pattern_match.group(1)
-      node = f'"{input}"'
-      output = subprocess.check_output(f"we-get --search {node} -J", shell=True)
-      nunku = f"Torrent Search Completed\nBelow is the list in JSON format\n\n{output}"
-      os.system('touch torrent.json')
-      with open("torrent.json",'w') as f:
-           f.write(str(nunku))
-      await event.client.send_file(
-            event.chat_id,
-            "torrent.json",
-            caption=f"Here is the list of torrents for **{input}**\n\nUse a good JSON reader.For eg: Acode")
-      os.system('rm -rf torrent.json')
+	if event.fwd_from:
+		return 
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+
+	search_str = event.pattern_match.group(1)
+
+	print(search_str)
+	await event.reply("Searching for "+search_str+".....")
+	if " " in search_str:
+		search_str = search_str.replace(" ","+")
+		print(search_str)
+		res = requests.get("https://www.torrentdownloads.me/search/?new=1&s_cat=0&search="+search_str,headers)
+
+	else:
+		res = requests.get("https://www.torrentdownloads.me/search/?search="+search_str,headers)
+
+	source = bs(res.text,'lxml')
+	with open("source.html",'w') as f:
+		f.write(str(source))
+
+	urls = []
+	magnets = []
+
+	counter = 0
+	for a in source.find_all('a',{'class':'cloud'}):
+		# print("https://www.torrentdownloads.me"+a['href'])
+		try:
+			urls.append("https://www.torrentdownloads.me"+a['href'])
+		except:
+			pass
+		if counter == 30:
+			break		
+		counter = counter + 1
+	if not urls:
+		await event.reply("Either the Keyword was restricted or not found..")		
+		return
+
+	print("Found URLS....")
+		
+	for url in urls:
+		res = requests.get(url,headers)
+		# print("URl: "+url)
+		source = bs(res.text,'lxml')
+		with open("source2.html",'w') as f:
+			f.write(str(source))
+		for div in source.find_all('div',{'class':'grey_bar1 back_none'}):
+			try:
+				mg = div.p.a['href']
+				# print(str(mg))
+				magnets.append("<b>URL: </b>"+str(url)+"<br/><b>\nMagnet: </b>{}\n<br/>".format(str(mg)))
+			except:
+				pass	
+	print("Found Magnets...")			
+	msg = ""
+	try:
+		search_str = search_str.replace("+"," ")
+	except:
+		pass	
+	for i in magnets:
+		msg = msg + i
+	response = telegraph.create_page(
+	search_str,
+	html_content = msg
+	)	
+	await event.reply('Magnet Links for {}:\nhttps://telegra.ph/{}'.format(search_str,response['path']))
 
 
 __help__ = """
