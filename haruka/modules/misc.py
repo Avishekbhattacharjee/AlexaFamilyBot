c860e283e29de47a786c8373d1bbd97d1ab77422
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-06 14:10:20 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -828,69 +828,6 @@ def ud(bot: Bot, update: Update, args):
         else:
             update.message.reply_text("Term not found")
 
-from telethon import events
-import datetime
-import io
-import speedtest
-
-def convert_from_bytes(size):
-    power = 2**10
-    n = 0
-    units = {
-        0: "",
-        1: "kilobytes",
-        2: "megabytes",
-        3: "gigabytes",
-        4: "terabytes"
-    }
-    while size > power:
-        size /= power
-        n += 1
-    return f"{round(size, 2)} {units[n]}"
-
-@register(pattern="^/ping$")
-async def _(event):
-    if event.fwd_from:
-        return
-    await event.reply("Checking the ping rate of @AlexaFamilyBot")
-    start = datetime.datetime.now()
-    s = speedtest.Speedtest()
-    s.get_best_server()
-    s.download()
-    s.upload()
-    end = datetime.datetime.now()
-    ms = (end - start).microseconds / 1000
-    response = s.results.dict()
-    download_speed = response.get("download")
-    upload_speed = response.get("upload")
-    ping_time = response.get("ping")
-    client_infos = response.get("client")
-    i_s_p = client_infos.get("isp")
-    i_s_p_rating = client_infos.get("isprating")
-    reply_msg_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_msg_id = event.reply_to_msg_id
-    try:
-        response = s.results.share()
-        speedtest_image = response
-        await event.reply("""**SpeedTest** completed in {} seconds
-Download: {}
-Upload: {}
-Ping: {}
-Internet Service Provider: {}
-ISP Rating: {}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
-        await tbot.send_file(
-                event.chat_id,
-                speedtest_image,
-                force_document=True,
-                reply_to=reply_msg_id,
-                allow_cache=False
-            )
-    except Exception as exc:
-        return
-
-
-
 @register(pattern="^/yt (.*)")
 async def yts_search(video_q):
     # For .yts command, do a YouTube search from Telegram.
