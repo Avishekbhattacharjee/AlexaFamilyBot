045c5f91d4fb5fdef30b68f6d1de46d840f66479
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-08 00:36:09 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1609,6 +1609,38 @@ async def tor_search(event):
 	)	
 	await event.reply('Magnet Links for {}:\nhttps://telegra.ph/{}'.format(search_str,response['path']))
 
+import sys
+import requests
+import json
+import time
+import urllib
+import os
+
+
+class config:
+	key = "fe65b94e78fc2e3234c1c6ed1b771abd" #go to https://numverify.com/
+
+@register(pattern=r'^/phone (.*)')
+async def wolfram(event): 
+    information = event.pattern_match.group(1)
+    number = information
+    api = "http://apilayer.net/api/validate?access_key=" + config.key + "&number=" + number + "&country_code=&format=1"
+    output = requests.get(api)
+    content = output.text
+    obj = json.loads(content)
+    country_code = obj['country_code']
+    country_name = obj['country_name']
+    location = obj['location']
+    carrier = obj['carrier']
+    line_type = obj['line_type']	
+    a = "Phone number: "+str(number)
+    b = "Country: " +str(country_code)
+    c = "Country Name: " +str(country_name)
+    d = "Location: " +str(location)
+    e = "Carrier: " +str(carrier)
+    f = "Device: " +str(line_type)
+    await event.reply(a\nb\nc\nd\ne\nf)
+
 
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
