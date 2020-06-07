651c84e2ce620a4bede2cbc9f2a791e28eee029a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-08 01:25:46 +0530

    Update phone.py

--- haruka/modules/phone.py
+++ haruka/modules/phone.py
@@ -1,5 +1,5 @@
 __help__ = """
-- /phone <number in international format": Check if the number really exists and returns information about it.If the number is fake then it will return null-type response 
+- /phone <number in international format>: Check if the number really exists and returns information about it.If the number is fake then it will return null-type response 
 
 Example: `/phone +9162XX93X805`, `/phone +1916X978XX1`
 """
