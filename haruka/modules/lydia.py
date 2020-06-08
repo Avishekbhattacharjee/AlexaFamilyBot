3c94907f3a7a0be7c392045be9bebb2637c49ec1
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-08 13:36:07 +0530

    Update lydia.py

--- haruka/modules/lydia.py
+++ haruka/modules/lydia.py
@@ -2,4 +2,4 @@ __help__ = """
  - /autochat: Type in reply to someone to activate on him/her. The bot will give auto replies to that person\nCan also be used in PM's
  - /stopchat: Type in reply to someone to deactivate on him/her. The bot will stop giving auto replies to that person.
 """
-__mod_name__ = "Lydia"
+__mod_name__ = "Chatterbot"
