be25c1fa609d0812bc82e94eca82893fbcabc153
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-08 09:14:48 +0530

    Create zipper.py

---
+++ haruka/modules/zipper.py
@@ -0,0 +1,6 @@
+__help__ = """
+ - /zip: reply to a telegram file to compressing in .zip format
+ - /unzip: reply to a telegram file to decompress it from the .zip format
+"""
+
+__mod_name__ = "Zip/Unzip"
