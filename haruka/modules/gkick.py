063e8c84ae3b74d105e1032a19a28f29865c9b0a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-08 14:39:55 +0530

    Delete gkick.py

--- haruka/modules/gkick.py
+++
@@ -1,5 +0,0 @@
-__help__ = """
-*ONLY FOR SUDO USERS*
- - /gkick: Type in reply to a person to globally kick that person across all the chats where this bot is admin
-"""
-__mod_name__ = "Global Kicks"
