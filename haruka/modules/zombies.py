f34548d84ece0634d4124508593e97cedba0e1d9
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-10 18:07:50 +0530

    Update zombies.py

--- haruka/modules/zombies.py
+++ haruka/modules/zombies.py
@@ -1,5 +1,6 @@
 __help__ = """
  - /zombies: Searches the number of deleted account in your group 
  - /zombies clean: Removes all deleted account from your group 
+ - /kickthefools: Kicks all members who are inactive since 1 week
 """
 __mod_name__ = "Zombies"
