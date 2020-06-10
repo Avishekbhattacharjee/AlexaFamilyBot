382844c368242b70381cbe0aea6d83d4acddfe4c
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-11 00:03:25 +0530

    Update ukinti.py

--- haruka/modules/ukinti.py
+++ haruka/modules/ukinti.py
@@ -4,66 +4,54 @@ from telethon.tl.types import UserStatusEmpty, UserStatusLastMonth, UserStatusLa
 from telethon.tl import functions, types
 from time import sleep
 import asyncio
+from telethon import *
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
 from haruka.events import register
 
-@register(pattern="^/kickthefools")
+KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
+
+
+@register(pattern=".kickthefools")
 async def _(event):
     if event.fwd_from:
         return
-    if event.is_private:
-        return False   
     chat = await event.get_chat()
-    if not (chat.admin_rights or chat.creator):
-            await event.reply("`I am not admin here!`")
-            return False
-    p = 0
-    b = 0
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not admin and not creator:
+        await event.reply("I am not admin here !")
+        return
     c = 0
-    d = 0
-    ee = []
     m = 0
     n = 0
     y = 0
-    w = 0
-    o = 0
-    q = 0
-    r = 0
+    w = 0   
     await event.reply("Searching Participant Lists...")
     async for i in event.client.iter_participants(event.chat_id):
-        p = p + 1
-        rights = ChatBannedRights(
-            until_date=None,
-            view_messages=True
-        )
+
         if isinstance(i.status, UserStatusLastMonth):
             m = m + 1
-            status, e = await ban_user(event.chat_id, i, rights)
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
             if not status:
                     try:
                         await event.reply("I need admin priveleges to perform this action!")
                     except:
                         pass
-                    ee.append(str(e))
             else:
-                    c = c + 1
+               c = c + 1
                     
         if isinstance(i.status, UserStatusLastWeek):
             w = w + 1
-            status, e = await ban_user(event.chat_id, i, rights)
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
             if not status:
                     try:
                         await event.reply("I need admin priveleges to perform this action!")
                     except:
-                        pass
-                    ee.append(str(e))
+                        pass                    
             else:
-                    c = c + 1
+               c = c + 1
+
     required_string = "Successfully Kicked **{}** users"
     await event.reply(required_string.format(c))
-        
-async def ban_user(chat_id, i, rights):
-    try:
-        await event.client(functions.channels.replyBannedRequest(chat_id, i, rights))
-        return True, None
-    except Exception as exc:
-        return False, str(exc)
