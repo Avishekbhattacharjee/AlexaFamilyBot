a76ead09ccdb7cb59c56400b7457285c147b32ea
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-10 09:53:18 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -94,17 +94,7 @@ if ENV:
     if ALLOW_EXCL:
        tg.CommandHandler = CustomCommandHandler
     tg.CommandHandler = GbanLockHandler
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    MONGOCLIENT = MongoClient(MONGO_DB_URI, 27017, serverSelectionTimeoutMS=1)
-    MONGO = MONGOCLIENT.haruka
-
-    def is_mongo_alive():
-      try:
-        MONGOCLIENT.server_info()
-      except BaseException:
-        return False
-      return True
-
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)   
 
     REDIS = StrictRedis(host='localhost', port=6379, db=0)
 
