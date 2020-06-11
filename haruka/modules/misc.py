bd014b04277e807d0822b57a027b0bf900b3b987
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-11 20:00:38 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1700,6 +1700,49 @@ async def pingme(pong):
     await up.edit("`Pong!\n%sms`" % (duration))
 
 
+from telethon import events
+from telethon.tl.types import UserStatusEmpty, UserStatusLastMonth, UserStatusLastWeek, UserStatusOffline, UserStatusOnline, UserStatusRecently, ChannelParticipantsKicked, ChatBannedRights
+from telethon.tl import functions, types
+from time import sleep
+import asyncio
+from telethon import *
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
+
+@register(pattern="^/kickthefools")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = await event.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not admin and not creator:
+        await event.reply("I am not admin here !")
+        return
+    c = 0
+    KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
+    await event.reply("Searching Participant Lists...")
+    async for i in event.client.iter_participants(event.chat_id):
+
+        if isinstance(i.status, UserStatusLastMonth):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1
+                    
+        if isinstance(i.status, UserStatusLastWeek):
+            status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
+            if not status:
+               return
+            else:
+               c = c + 1                    
+
+    required_string = "Successfully Kicked **{}** users"
+    await event.reply(required_string.format(c))
+
+
 
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
