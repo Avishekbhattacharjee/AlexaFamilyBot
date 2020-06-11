aa9bf37d9363eb7b2cc4592b5e80d26f79e169ca
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-11 10:32:00 +0530

    Update ukinti.py

--- haruka/modules/ukinti.py
+++ haruka/modules/ukinti.py
@@ -11,7 +11,7 @@ from telethon.tl.functions.channels import (EditAdminRequest,
 from haruka.events import register
 
 KICK_RIGHTS = ChatBannedRights(until_date=None, view_messages=True)
-
+c = 0
 
 @register(pattern="^/kickthefools")
 async def _(event):
@@ -23,35 +23,23 @@ async def _(event):
     if not admin and not creator:
         await event.reply("I am not admin here !")
         return
-    c = 0
-    m = 0
-    n = 0
-    y = 0
-    w = 0   
+
     await event.reply("Searching Participant Lists...")
     async for i in event.client.iter_participants(event.chat_id):
 
         if isinstance(i.status, UserStatusLastMonth):
-            m = m + 1
             status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
             if not status:
-                    try:
-                        await event.reply("I need admin priveleges to perform this action!")
-                    except:
-                        pass
+               await event.reply("I need admin priveleges to perform this action!")
             else:
                c = c + 1
                     
         if isinstance(i.status, UserStatusLastWeek):
-            w = w + 1
             status = await event.client(EditBannedRequest(event.chat_id, i, KICK_RIGHTS))
             if not status:
-                    try:
-                        await event.reply("I need admin priveleges to perform this action!")
-                    except:
-                        pass                    
+               await event.reply("I need admin priveleges to perform this action!")
             else:
-               c = c + 1
+               c = c + 1                    
 
     required_string = "Successfully Kicked **{}** users"
     await event.reply(required_string.format(c))
