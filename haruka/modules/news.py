3ee7be4a618cd8244c571f8f9e4cb926557010dc
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-15 20:49:41 +0530

    Update news.py

--- haruka/modules/news.py
+++ haruka/modules/news.py
@@ -1,6 +1,6 @@
 __help__ = """
 *NOTE: THE MODULE RETURNS MAINLY INDIAN NEWS HEADLINES, MAY INCLUDE FOREIGN NEWS ALSO IN SOME CASES*
 
- - /news: Returns today's headlines
+ - /news: Returns today's headlines, ONLY WORKS IN PM
 """
 __mod_name__ = "News"
