c00bc4c1ab6304570af2999fce7d56b54c8ca2b8
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-17 12:14:17 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1675,13 +1675,9 @@ async def chat_bot(event):
       reply_msg = await event.get_reply_message()
       chats = auto_chat.find({})
       for c in chats:
-              if not event.chat_id == c['id'] and reply_msg.from_id == c['user']:
-                 await event.reply("This User is not in Auto-Chat List.")
-                 return
-              else:
-                 auto_chat.delete_one({'id':event.chat_id,'user':reply_msg.from_id})
-                 await event.reply("Chatterbot module turned off For User: "+str(reply_msg.from_id)+" in this chat.")
-
+        if event.chat_id == c['id'] and reply_msg.from_id == c['user']:
+           auto_chat.delete_one({'id':event.chat_id,'user':reply_msg.from_id})
+           await event.reply("Chatterbot module turned off For User: "+str(reply_msg.from_id)+" in this chat.")
 
 
 @register(pattern="")
