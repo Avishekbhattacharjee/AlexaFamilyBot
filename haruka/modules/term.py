f3acde34171d5635ac07bf8cb83e85e1effcad4a
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-18 18:08:51 +0530

    Delete term.py

--- haruka/modules/term.py
+++
@@ -1,4 +0,0 @@
-__help__ = """
- - /term <commands>: A micro terminal environment for executing basic commands.Delimeters should be in a single line !\n\nExample Usage: `/term date`\n\nFor multiple delimeters use *&&* eg: `term echo "hi, how are you ?" > hi.txt && cat hi.txt` 
-"""
-__mod_name__ = "Terminal"
