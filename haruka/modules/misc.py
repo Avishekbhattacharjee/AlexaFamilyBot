09f73e7f8595239bad6454dd33f0034d153f90f4
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-21 22:49:19 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1590,6 +1590,58 @@ async def list_db(event):
 	await event.reply(msg)
 
 
+import cv2
+import numpy as np
+import mapper
+import os
+
+@register(pattern="^/camscanner")
+async def asciiart(event):
+  if event.fwd_from:
+     return  
+  if not event.from_id:
+     await event.reply("Reply To A Image Plox..")
+     return
+  reply_msg = await event.get_reply_message()
+  downloaded_file_name = await event.client.download_media(reply_msg, './')
+
+  image=cv2.imread(downloaded_file_name)   #read in the image
+  image=cv2.resize(image,(1300,800)) #resizing because opencv does not work well with bigger images
+  orig=image.copy()
+  gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+  cv2.imshow("Title",gray)
+
+  blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+  cv2.imshow("Blur",blurred)
+
+  edged=cv2.Canny(blurred,30,50)  #30 MinThreshold and 50 is the MaxThreshold
+  cv2.imshow("Canny",edged)
+
+  contours,hierarchy=cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)  #retrieve the contours as a list, with simple apprximation model
+  contours=sorted(contours,key=cv2.contourArea,reverse=True)
+
+  for c in contours:
+    p=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c,0.02*p,True)
+
+    if len(approx)==4:
+        target=approx
+        break
+  approx=mapper.mapp(target) #find endpoints of the sheet
+
+  pts=np.float32([[0,0],[800,0],[800,800],[0,800]])  #map to 800*800 target window
+
+  op=cv2.getPerspectiveTransform(approx,pts)  #get the top or bird eye view effect
+  dst=cv2.warpPerspective(orig,op,(800,800))
+
+  cv2.imwrite("./scanned.pdf", dst)
+
+  omg = await event.client.upload_file('./scanned.pdf')
+
+  await event.client.send_file(event.chat_id, omg)
+  os.remove("./scanned.pdf")
+
+
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
  - /runs: reply a random string from an array of replies.
