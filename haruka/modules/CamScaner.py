1fc9175ca238494ab984ed0211250c310c669da3
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-22 15:25:56 +0530

    Update CamScaner.py

--- haruka/modules/CamScaner.py
+++ haruka/modules/CamScaner.py
@@ -1,4 +1,13 @@
 __help__ = """
  - /camscanner: Reply to a image to scan and improve it's clarity.
+
+*Instructions*
+▪️The image should be a page with some written text on it (screenshots aren't permitted)
+▪️The image should contain the page with four corners clearly visible
+▪️The background should be somewhat darker than the page
+▪️The image should contain only the page with no other objects like pencil, eraser etc. beside it(within the image)
+
+*Examples*
+See [The Original Image](https://telegra.ph/file/892b85d31f4aeed2f14c8.jpg) and the [The Scanned Image](https://telegra.ph/file/53231747269bd72d186ad.jpg)
 """
 __mod_name__ = "CamScanner"
